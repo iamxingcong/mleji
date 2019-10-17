5ae173fc2117b5fafbc7d24807c33b5b700a068e
unknown <121950263@qq.com>
2019-10-17 12:04:16 +0800

    update pages

--- src/components/page/DataManage.vue
+++ src/components/page/DataManage.vue
@@ -1,10 +1,34 @@
 <template>
   <div id='DataManage'>
-    <span>数据管理</span>
+
+    <el-row>
+      <span class="wd400">
+        <el-input v-model="input" placeholder="请输入搜索关键词"></el-input>
+      </span>
+        <el-button type="primary">搜索音乐</el-button>
+        <el-button type="primary">全部音乐</el-button>
+        <el-button plain>刷新</el-button>
+    </el-row>
+
   </div>
 </template>
 <script>
 export default {
-  name: 'DataManage'
+  name: 'DataManage',
+  data () {
+    return {
+      input: ''
+    }
+  }
 }
 </script>
+<style scoped>
+  .el-row span, .el-row .el-button--primary{
+    display: block;
+    float: left;
+  }
+  .wd400{
+    width: 400px;
+    margin-right: 10px;
+  }
+</style>
